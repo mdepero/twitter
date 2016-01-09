96157bdeabf5d8a3538fdadeee7ce9741a93ce5d
Matt DePero <deperomm@miamioh.edu>
2016-01-09 09:13:20 -0500

    testing scripts setup

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,25 +1,58 @@
 // Your scripts go here.
 
 
-$(document).ready(function(){
-  $('a[href*=#]').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-    && location.hostname == this.hostname) {
-      var $target = $(this.hash);
-      $target = $target.length && $target
-      || $('[name=' + this.hash.slice(1) +']');
-      if ($target.length) {
-        var targetOffset = $target.offset().top;
-        $('html,body')
-        .animate({scrollTop: targetOffset}, 1000);
-       return false;
-      }
+// URL to folder that contains serverfile.php, including '/' on the end
+var serverRootURL = "http://ohioporcelain.com/twitter.php";
+
+
+var xmlhttp;
+if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
+
+  xmlhttp=new XMLHttpRequest();
+}else{// code for IE6, IE5
+
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+}
+
+
+
+
+var returnedData;
+
+function fetchData( callback, v1, v2 ){
+  var url = serverRootURL+"serverfile.php?q=" + callback.getName() + "&v1=" +v1+ "&v2=" +v2+ "&t=" + Math.random();
+  url = url.replace(" ","%20");
+  console.log("SENT URL: "+url);
+  xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            returnedData = xmlhttp.responseText;
+            console.log("RECEIVED DATA: "+returnedData);
+            if(returnedData == "" || returnedData == null){
+              alert("ERROR: Could not connect or returned no results.");
+              return;
+            }
+
+            returnedData = JSON.parse(returnedData);
+
+            callback();
+        }
     }
-  });
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
 
 
-  $('.carousel').carousel({
-	  interval: 6000
-	})
 
-});
+document.onload = function(){
+
+  // hard code group id for now
+  fetchData(getdata,1,"");
+
+}
+
+function getdata(){
+
+  alert(returnedData);
+
+}
+
